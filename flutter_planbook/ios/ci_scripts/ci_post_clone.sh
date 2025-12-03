0aebb0a7969485fe8110ffde9a6c121c55a86843
Bapaws <dev@bapaws.com>
2025-12-03 21:40:25 +0800

    main

--- flutter_planbook/ios/ci_scripts/ci_post_clone.sh
+++ flutter_planbook/ios/ci_scripts/ci_post_clone.sh
@@ -5,10 +5,15 @@ set -e
 
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+cd flutter_planbook
 
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
+
+# Current directory
+echo "Current directory after change: $(pwd)"
+echo "Repository path: $CI_PRIMARY_REPOSITORY_PATH"
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
@@ -22,5 +27,8 @@ brew install cocoapods
 
 # Install CocoaPods dependencies.
 cd ios && pod install # run `pod install` in the `ios` directory.
+
+# https://github.com/flutter/flutter/issues/174015
+flutter build ios --config-only --release
 
 exit 0
--- flutter_planbook/lib/main.dart
+++ flutter_planbook/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_planbook/bootstrap.dart';
 
 void main() {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   bootstrap();
 }
